fa2ee5f5e0e1c7debde90e05414360a81b2c7eba
Connor <connordbrown@gmail.com>
2023-10-31 16:26:05 -0600

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibSeq = []
+    elif length == 1:
+        fibSeq = [0]
+    elif length == 2:
+        fibSeq = [0, 1]
+    else:
+        fibSeq = [0, 1]
+        for i in range(length - 2):
+            fibSeq.append(fibSeq[i] + fibSeq[i+1])
+    print(fibSeq)
